b026fabe169ecaf9b71d6a81c0231b47aa972feb
Oleg Solomka <legomushroom@gmail.com>
2016-01-16 14:15:52 +0200

    add Fb pixel tracking code

--- header.php
+++ header.php
@@ -8,6 +8,21 @@
 		<!-- End WP Header -->
 		<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
 		
+		<!-- Facebook Pixel Code -->
+		<script>
+		!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+		n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+		document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+		fbq('init', '1659014534386292');
+		fbq('track', "PageView");</script>
+		<noscript><img height="1" width="1" style="display:none"
+		src="https://www.facebook.com/tr?id=1659014534386292&ev=PageView&noscript=1"
+		/></noscript>
+		<!-- End Facebook Pixel Code -->
+		
 	</head>
 	<body id="site-top" <?php body_class(); ?>>
 		<div id="fb-root"></div>
