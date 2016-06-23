07de137f5dc4619e198d8b7bff5f2bddb28d768f
Oleg Solomka <legomushroom@gmail.com>
2016-06-23 12:51:35 -0500

    modal window: fix `lost your password styles`

--- functions.php
+++ functions.php
@@ -954,15 +954,15 @@ function acf_update_shares_forgery ($value, $post_id, $field) {
 /* -----------------------------------------------------------------------------
  * CUSTOM: Recalculate total shares with forgery.
  * -------------------------------------------------------------------------- */
-  // removes Order Notes Title - Additional Information
-  add_filter( 'woocommerce_enable_order_notes_field', '__return_false' );
-  //remove Order Notes Field
-  add_filter( 'woocommerce_checkout_fields' , 'remove_order_notes' );
+// removes Order Notes Title - Additional Information
+add_filter( 'woocommerce_enable_order_notes_field', '__return_false' );
+//remove Order Notes Field
+add_filter( 'woocommerce_checkout_fields' , 'remove_order_notes' );
 
-  function remove_order_notes( $fields ) {
-       unset($fields['order']['order_comments']);
-       unset($fields['billing']['billing_company']);
-       unset($fields['billing']['billing_phone']);
+function remove_order_notes( $fields ) {
+     unset($fields['order']['order_comments']);
+     unset($fields['billing']['billing_company']);
+     unset($fields['billing']['billing_phone']);
 
-       return $fields;
-  }
+     return $fields;
+}
